4ce539262d305b0baa4aaabb7eca6014fce89a59
Link <joahnnsilvamendez@gmail.com>
2025-10-30 08:30:44 -0500

    mi changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+import swaggerUI from   "swagger-ui-express"
 import "dotenv/config"
 //import signUp from "./routes/SignUp.js"
 import users from "./routes/Users.js"
@@ -8,6 +9,10 @@ import users from "./routes/Users.js"
 const app = express()
 app.use(express.json());
 app.use(cors())
+app.use("/docs",swaggerUI.serve,swaggerUI.setup())
+/*swager mediante un ruta genera una adocumentacion,
+con el metodo serve proporciona un handler para manejar documentacion en el archivo que pide swager
+setup() en esta funcion definiremos un objeto de configuracion de datos predeterminados para swager*/
 app.use("/api/usuarios-colegio", users);
 //app.use("/api/usuarios-colegio", signUp);
 
